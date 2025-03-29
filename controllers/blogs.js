e58f871572707fe86008919d37bf92d660e66b81
Lehlohonolo Motshoane <motshoanelehlohonolo67@gmail.com>
2025-03-29 13:51:43 +0200

    test(blog): add tests for DELETE and PUT endpoints

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,4 +26,45 @@ blogsRouter.post('/', async (req, res) => {
     res.status(201).json(savedBlog)
 })
 
+blogsRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const deletedBlog = await Blog.findByIdAndDelete(id)
+        
+        if (!deletedBlog) {
+            return res.status(404).json({ error: 'Blog not found' })
+        }
+
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json({ error: 'Malformed ID' })
+    }
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { likes } = req.body
+
+    if (likes === undefined) {
+        return res.status(400).json({ error: 'Likes are required to update' })
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            id, 
+            { likes },
+            { new: true, runValidators: true, context: 'query' }
+        )
+
+        if (!updatedBlog) {
+            return res.status(404).json({ error: 'Blog not found' })
+        }
+
+        res.json(updatedBlog)
+    } catch (error) {
+        res.status(400).json({ error: 'Malformed ID' })
+    }
+})
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -98,6 +98,57 @@ describe('4.12: Missing title or URL should return 400', () => {
   })
 })
 
+describe('4.13: DELETE /api/blogs/:id', () => {
+  test('successfully deletes a blog post with a valid id', async () => {
+    const blogsAtStart = await Blog.find({})
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await Blog.find({})
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map((b) => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+
+  test('fails with statuscode 400 if id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    await api.delete(`/api/blogs/${invalidId}`).expect(400)
+  })
+})
+
+describe('4.14: PUT /api/blogs/:id', () => {
+  test('successfully updates likes for a blog post', async () => {
+    const blogsAtStart = await Blog.find({})
+    const blogToUpdate = blogsAtStart[0]
+
+    const updatedBlog = {
+      ...blogToUpdate,
+      likes: blogToUpdate.likes + 1,
+    }
+
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(updatedBlog.likes)
+
+    const blogsAtEnd = await Blog.find({})
+    const updatedBlogInDb = blogsAtEnd.find((b) => b.id === blogToUpdate.id)
+    expect(updatedBlogInDb.likes).toBe(updatedBlog.likes)
+  })
+
+  test('fails with statuscode 400 if id is invalid for update', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    await api.put(`/api/blogs/${invalidId}`).send({ likes: 100 }).expect(400)
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
